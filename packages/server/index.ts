984d8558db7315a0ef18e580a51f727b5f259cbc
Thomas <tbe@pm.me>
2025-10-29 10:02:09 +0100

    setup prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,20 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const apiKey = process.env.OPENAI_API_KEY || "apiKey is missing...";
+const apiKey = process.env.OPENAI_API_KEY || 'apiKey is missing...';
 
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello World!!");
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello World!!');
 });
 
-app.get("/api/hello", (req: Request, res: Response) => {
-	res.json({ message: "Hallo World!" });
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hallo World!' });
 });
 
 app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
