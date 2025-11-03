906fd2d8ac787fc52c4e0f07ad6384bf45b0dd9e
Thomas <tbe@pm.me>
2025-11-03 08:36:29 +0100

    creating frontend - styling the client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,10 @@
-import { useEffect, useState } from 'react';
-import { Button } from './components/ui/button';
+import ChatBot from './components/chatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
    return (
       <>
          <div className="p-4">
-            <p className="font-bold text-3xl">{message}</p>
-            <Button>Click me!</Button>
+            <ChatBot></ChatBot>
          </div>
       </>
    );
